0856de98601cbfa67ddb78b12b9ea819b34cc638
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-05-15 10:48:46 -0300

    Update ambiente-dev-ubuntu.sh

--- ambiente-dev-ubuntu.sh
+++ ambiente-dev-ubuntu.sh
@@ -49,3 +49,30 @@ git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-m
 fc-cache -vf
 
 # REBOOT!!!!!!!!!!!!!!!!!!!!!
+
+### [Gnome Terminal](https://wiki.gnome.org/Apps/Terminal)
+
+#### Install using Git
+
+If you are a git user, you can install the theme by cloning the repo:
+
+    $ git clone https://github.com/getomni/gnome-terminal.git
+
+#### Install manually
+
+Download using the [GitHub .zip download](https://github.com/getomni/gnome-terminal/archive/main.zip) option and unzip them.
+    
+#### Activating theme
+
+1. You will need the `dconf` command:
+
+    ```bash 
+    $ sudo apt-get install dconf-cli 
+    ```
+
+2. Then you can run the installation script:
+
+    ```bash
+    $ cd gnome-terminal
+    $ ./install.sh
+    ```
